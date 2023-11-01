3c96340b51e9cdfef3ae06d0e659c7999d22ef59
Jason <jasongitmail@gmail.com>
2023-11-01 23:54:34 +0000

    try alternate export syntax

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -3,5 +3,9 @@ import type { ParamValues, SitemapConfig } from './sitemap.js';
 import { sampledPaths, sampledUrls } from './sampled.js';
 import { response } from './sitemap.js';
 
-export type { ParamValues, SitemapConfig };
-export { response, sampledPaths, sampledUrls };
+export type { ParamValues };
+export type { SitemapConfig };
+
+export { response };
+export { sampledPaths };
+export { sampledUrls };
