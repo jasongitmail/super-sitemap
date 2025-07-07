f637cc64810d9032b3bd7415cccd7f40eae7542c
Jason <jasongitmail@gmail.com>
2025-07-07 19:05:14 +0000

    clean up types

--- src/lib/sitemap.ts
+++ src/lib/sitemap.ts
@@ -15,8 +15,8 @@ export type Priority = 0.0 | 0.1 | 0.2 | 0.3 | 0.4 | 0.5 | 0.6 | 0.7 | 0.8 | 0.9
 
 /* eslint-disable perfectionist/sort-object-types */
 export type SitemapConfig = {
-  additionalPaths?: [] | string[];
-  excludeRoutePatterns?: [] | string[];
+  additionalPaths?: string[];
+  excludeRoutePatterns?: string[];
   headers?: Record<string, string>;
   lang?: {
     default: string;
